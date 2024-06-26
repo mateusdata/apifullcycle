203adc22c00d3bcd2da79ecde28c5c5c56a5b037
Mateus Santos <mateuspele2015@gmail.com>
2024-06-26 06:17:58 -0300

    features: configure limit rate

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,26 @@ import connectDatabase from './config/conection';
 import todolistRoutes from './routes/todolistRoutes';
 import authRoute from './routes/authRoutes';
 import fastifyExpress from '@fastify/express';
+import fastifyRateLimit from '@fastify/rate-limit';
 const PORT = process.env.PORT || 3000;
 const ADDRESS = '0.0.0.0';
 
-const app = fastify()
+const app = fastify({
+    bodyLimit: 1024 * 1024 * 5,
+    trustProxy: true,
+    logger: true,
+
+});
+
 connectDatabase()
+
+app.register(fastifyRateLimit), {
+    max: 100,
+    timeWindow: '1 minute',
+}
+
+app.register(fastifyRateLimit, { global: true, max: 20, timeWindow: 1000 *60 })
+
 app.register(fastifyExpress);
 
 
