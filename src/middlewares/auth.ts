f9db8df01bf7c8f93868fb81634db980f89c25aa
Mateus Santos <mateuspele2015@gmail.com>
2024-07-02 12:43:06 -0300

    refactor: auth middeware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import jwt, { TokenExpiredError } from 'jsonwebtoken';
 
-export default async function authenticateToken(request: FastifyRequest, reply: FastifyReply) {
-  const { headers } = request;
-  const tokenHeader = headers['authorization'];
+export default async function auth(request: FastifyRequest, reply: FastifyReply) {
+  const { headers, query }:any = request;
+  const tokenHeader = query['authorization'];
+  console.warn(tokenHeader)
 
   if (!tokenHeader) {
     return reply.status(403).send("Access Unauthorized");
   }
 
-  const token = tokenHeader.split(" ")[1];
   try {
 
-    jwt.verify(token, "123", (err:any, decode:any) => {
+    jwt.verify(tokenHeader, "123", (err:any, decode:any) => {
 
       if (err) {
         return reply.status(401).send({ message: "Token is invalid or expired" });
@@ -26,6 +26,6 @@ export default async function authenticateToken(request: FastifyRequest, reply: 
 
   } catch {
 
-    return reply.status(500).send({ message: "Internal erorr", token });
+    return reply.status(500).send({ message: "Internal erorr", tokenHeader });
   }
 }
--- src/routes/todolistRoutes.ts
+++ src/routes/todolistRoutes.ts
@@ -3,13 +3,14 @@ import TodolistController from '../controllers/TodolistController';
 import { prisma } from '../config/conection';
 import authenticateToken from '../middlewares/authenticateToken';
 import UserController from '../controllers/UserController';
+import auth from '../middlewares/auth';
 
 export default async function todolistRoutes(route: FastifyInstance) {
   // Middleware para verificar o token de autenticação
   //route.addHook('preHandler', authenticateToken);
 
   // Rotas do controlador Todolist
-  //route.post('/todolists', { preHandler: authenticateToken }, TodolistController.create);
+  //route.get('/todolist', { preHandler: auth }, TodolistController.index);
   
   route.post('/todolist', TodolistController.create);
   route.get('/todolist', TodolistController.index);
